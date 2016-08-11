d9016dd45e75e85e2a1c774823e36b00b67ffd7f
Koen van Gilst <koen.vangilst@gmail.com>
2016-08-11 21:37:15 +0200

    adds check on double donations on same date

--- api/controllers/DonationController.js
+++ api/controllers/DonationController.js
@@ -1,9 +1,27 @@
-const Donation = require('../models/Bot');
+const Donation = require('../models/Donation');
 const Bot = require('../models/Bot');
 const asyncFn = require('asyncawait/async');
 const awaitFn = require('asyncawait/await');
+const moment = require('moment');
 
 exports.createDonation = asyncFn((donationData) => {
+	const createdAt = donationData.createdAt;
+	const earlierDonation = awaitFn(
+		Donation.find({
+			fromTwitterID: donationData.fromTwitterID,
+			createdAt: {
+				$gte: moment(createdAt)
+					.startOf('day')
+					.toDate(),
+				$lt: moment(createdAt)
+					.endOf('day')
+					.toDate(),
+			},
+		})
+	);
+	if (earlierDonation.length) {
+		throw new Error('Donation not allowed. Already donated today!');
+	}
 	const donation = new Donation(donationData);
 	const bot = awaitFn(Bot.findOne());
 	bot.money += donation.money;
--- test/donation-create-test.js
+++ test/donation-create-test.js
@@ -6,10 +6,12 @@
 const app = require('../server');
 const test = require('tape');
 const Bot = require('../api/models/Bot');
+const Donation = require('../api/models/Donation');
 const BotController = require('../api/controllers/BotController');
 const DonationController = require('../api/controllers/DonationController');
 const asyncFn = require('asyncawait/async');
 const awaitFn = require('asyncawait/await');
+const moment = require('moment');
 
 const {
 	cleanup,
@@ -21,23 +23,54 @@ const botData = {
 };
 
 const donationData = {
-	twitterID: 'abcde',
+	fromTwitterID: 'abcde',
+	createdAt: moment()
+		.subtract(1, 'days')
+		.toDate(),
 	money: 10,
 };
 
 test('Clean up', cleanup);
 
-test('Creating donation', asyncFn(t => {
-	awaitFn(BotController.createOrFindBot(botData));
-	const createdDonation = awaitFn(
-		DonationController
-		.createDonation(donationData));
-	t.ok(createdDonation, 'A donation should be created');
-	const bot = awaitFn(Bot.findOne());
-	t.ok(bot, 'A bot should be found in db');
-	t.equal(bot.money, donationData.money, 'Bot should have 10 monies');
-	t.end();
-}));
+test('Creating donation',
+	asyncFn(t => {
+		awaitFn(BotController.createOrFindBot(botData));
+		const createdDonation = awaitFn(
+			DonationController
+			.createDonation(donationData));
+		t.ok(createdDonation, 'A donation should be created');
+		const bot = awaitFn(Bot.findOne());
+		t.ok(bot, 'A bot should be found in db');
+		t.equal(bot.money, 10, 'Bot should have 10 monies');
+		t.end();
+	}));
+
+test('Creating another donation with same date',
+	asyncFn(t => {
+		try {
+			awaitFn(DonationController
+				.createDonation(donationData));
+		} catch (err) {
+			t.equal(err.message,
+				'Donation not allowed. Already donated today!',
+				'Should throw an error: already donated today.');
+		}
+		const bot = awaitFn(Bot.findOne());
+		t.equal(bot.money, 10, 'Bot should still have 10 monies');
+		t.end();
+	}));
+
+test('Creating a donation with a different date',
+	asyncFn(t => {
+		donationData.createdAt = new Date();
+		awaitFn(DonationController
+			.createDonation(donationData));
+		const bot = awaitFn(Bot.findOne());
+		t.equal(bot.money, 20, 'Bot should  have 10 monies');
+		const donations = awaitFn(Donation.find());
+		t.equal(donations.length, 2, 'There should be 2 valid donation in the db');
+		t.end();
+	}));
 
 test('Clean up', cleanup);
 
