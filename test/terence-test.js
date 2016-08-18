e1ce86440918c7eae5ac5881f1f0851a444f0d5a
Koen van Gilst <koen.vangilst@gmail.com>
2016-08-18 12:28:02 +0200

    adds tests for terence

---
+++ test/terence-test.js
@@ -0,0 +1,70 @@
+const test = require('tape');
+const Terence = require('../bot/terence');
+const app = require('../server');
+// const asyncFn = require('asyncawait/async');
+// const awaitFn = require('asyncawait/await');
+
+const config = {
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	access_token: process.env.TWITTER_ACCESS_TOKEN,
+	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+	timeout_ms: 60 * 1000,
+};
+
+let terence;
+
+test('Creating Terence', t => {
+	terence = new Terence(config);
+	t.pass('Terence running.');
+	t.end();
+});
+
+test('Creating and deleting a tweet', t => {
+	let tweetId = null;
+	const status = 'Hello world, this is a test tweet';
+
+	terence.tweet(status)
+		.then((result) => {
+			tweetId = result.data.id_str;
+			t.ok(tweetId, 'a tweet should be created');
+			return terence.destroy(tweetId);
+		})
+		.then((result) => {
+			t.ok(result, 'a tweet should be deleted');
+			t.end();
+		})
+		.catch((err) => {
+			t.notOk(err, 'there should not be an error');
+			t.comment(err);
+			t.end();
+		});
+});
+
+test('Find a random tweet about bots and follow user, unfollow after that', t => {
+	terence.searchFollow({
+		q: 'bot',
+		lang: 'en',
+		count: 100,
+	})
+		.then((result) => {
+			const target = result.data.id_str;
+			t.ok(target, 'should follow user');
+			t.comment(`Following @${result.data.screen_name}`);
+			return terence.twit.post('friendships/destroy', {
+				id: target,
+			});
+		})
+		.then((result) => {
+			t.ok(result.data, 'should unfollow user');
+			t.comment(`Unfollowing @${result.data.screen_name}`);
+			t.end();
+		})
+		.catch((err) => {
+			t.notOk(err, 'there should not be an error');
+			t.comment(err);
+			t.end();
+		});
+});
+
+test.onFinish(() => process.exit(0));
